932de9c6ab528bcad0a251b4cdb905a475d84d6d
ishriom53tyagi <17bcs053@smvdu.ac.in>
2020-08-09 22:02:09 +0530

    some changes

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -599,6 +599,7 @@ $(document).ready(function (){
                     code: $('#discountCode').val(),
                     type: $('#discountType').val(),
                     value: $('#discountValue').val(),
+                    minimum: $('#discountMinimum').val(),
                     start: $('#discountStart').val(),
                     end: $('#discountEnd').val()
                 }
