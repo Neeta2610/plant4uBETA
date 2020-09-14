ca0b4125633aa2c40eecd20bab3fd3645c3b4ee8
root <skkaushik212@gmail.com>
2020-09-14 15:44:13 +0530

    Changes

--- routes/index.js
+++ routes/index.js
@@ -849,20 +849,20 @@ await mailer.sendEmail('admin@plant4u.com',req.session.customerEmail,'Order Comp
     
 // Here we send whatsapp message to vendor whenever we have an order
     var detailsmessage = "Name: ".concat(bold(order.orderFirstname)).concat(" ").concat(bold(order.orderLastname));
-    detailsmessage = detailsmessage.concat("\n Email: ").concat(order.orderEmail);
-    detailsmessage = detailsmessage.concat("\n Phone: ").concat(order.orderPhoneNumber);
+    detailsmessage = detailsmessage.concat(" Email: ").concat(order.orderEmail);
+    detailsmessage = detailsmessage.concat(" Phone: ").concat(order.orderPhoneNumber);
     var address = "Address: ".concat(order.orderAddr1).concat(" ").concat(order.orderState).concat(" ").concat(order.orderPostcode);
     var items = ``;
         for(let key in order.orderProducts){
-            items += `\n Product:- `+bold(order.orderProducts[key].title)+`, Quantity:- `+bold(order.orderProducts[key].quantity.toString())+``;
+            items += ` Product:- `+bold(order.orderProducts[key].title)+`, Quantity:- `+bold(order.orderProducts[key].quantity.toString())+``;
         }
     var sendmessage = `Your next order of `+items+` has shipped and should be delivered on `+address+`. Details: `+detailsmessage+``;
     client.messages.create({
         from:'whatsapp:+14155238886',
-        to:'whatsapp:+919910160442',
+        to:'whatsapp:+917889896521',
         body:sendmessage
     }).then(message=> console.log(message));
-
+    
     res.render('success', {
         title: 'Payment complete',
         config: req.app.config,
