dc051a372aa82641625c206dc55e0030ac83d3fc
root <skkaushik212@gmail.com>
2020-10-30 23:47:04 +0530

    changes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
     if ($('.menu-trigger').length) {
       $(".menu-trigger").on('click', function () {
         $(this).toggleClass('active');
-        $('.header-area .nav').slideToggle(200);
+        $('.header-area .nav').toggleClass('slide');
       });
     }
   
